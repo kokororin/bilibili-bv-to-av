e86522e693754f9bc84a16a795bc9f87113cf3fa
kokororin <i@kotori.love>
2020-03-25 11:46:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bilibili BV to av
+
+## Install
+https://chrome.google.com/webstore/detail/ijepcdfnlcdepamjdjcfgnphjfcpbapc
+
+## Intro
+Convert BV to av for bilibili.com.
+
+## Thanks
+https://www.zhihu.com/question/381784377/answer/1099438784
---
+++ inject.js
@@ -0,0 +1,36 @@
+var table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF',
+  tr = {};
+for (var i = 0; i < 58; i++) {
+  tr[table[i]] = i;
+}
+var s = [11, 10, 3, 8, 4, 6],
+  xor = 177451812,
+  add = 8728348608;
+
+function dec(x) {
+  var r = 0;
+  for (var i = 0; i < 6; i++) {
+    r += tr[x[s[i]]] * 58 ** i;
+  }
+  return (r - add) ^ xor;
+}
+
+function enc(x) {
+  x = (x ^ xor) + add;
+  r = 'BV1  4 1 7  '.split('');
+  for (var i = 0; i < 6; i++) {
+    r[s[i]] = table[Math.floor(x / 58 ** i) % 58];
+  }
+  return r.join('');
+}
+
+var url = window.location.pathname + window.location.search;
+
+url = url.replace(/\/video\/(BV([a-zA-Z0-9]+))/, function(str, bv, bvCode) {
+  var avCode = dec(bv);
+  return str.replace(bv, 'av' + avCode);
+});
+
+if (/\/video\/av[0-9]+/.test(url)) {
+  window.history.replaceState(null, null, url);
+}
